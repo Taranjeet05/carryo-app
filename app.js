99236e9c24752ac6b1cecfa1d56b8b39ef12c151
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-17 16:38:29 +0200

    chore: set up middleware and view engine in app.js

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8001;
 
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.send(
     "Hare Krishna Hare Krishna, Krishna Krishna Hare Hare, Hare Rama Hare Rama, Rama Rama Hare Hare"
