5227c9ec8016120ea58034fc1c8432c460aa6a56
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-20 12:19:22 +0200

    feat: update index route to include error handling

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use("/products", productsRouter);
 /* I am creating some routes for testing out ejs*/
 
 app.get("/", (req, res) => {
-  res.render("index"); // Render the index.ejs file
+  res.render("index", {error : ''}); // Render the index.ejs file
 });
 
 app.get("/header", (req, res) => {
