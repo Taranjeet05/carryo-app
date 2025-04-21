b4f3405bb76760849a8d26d9f77ed3563dafd5a6
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-21 02:45:14 +0200

    feat: implement user registration controller with validation and token generation

---
+++ controllers/authControllers.js
@@ -0,0 +1,55 @@
+const debug = require("debug")("development: authControllers");
+const { z } = require("zod");
+const userModel = require("../models/user-model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { genrateToken } = require("../utils/genrateToken");
+
+const userRegistrationSchema = z.object({
+  userName: z.string().trim().min(3, "Username is Required"),
+  email: z
+    .string()
+    .trim()
+    .email("Invalid Email Format")
+    .transform((email) => email.toLowerCase()),
+  password: z
+    .string()
+    .trim()
+    .min(6, "Password must be at least 6 characters long"),
+});
+
+module.exports.registerUser = async (req, res) => {
+  try {
+    const parsed = userRegistrationSchema.safeParse(req.body);
+
+    if (!parsed.success) {
+      return res.status(400).json({
+        error: parsed.error.errors.map((error) => error.message),
+      });
+    }
+
+    const { userName, email, password } = parsed.data;
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const user = await userModel.findOne({ email });
+    if (user)
+      return res
+        .status(401)
+        .send("You already have an account with this Email.");
+
+    const createUser = await userModel.create({
+      userName,
+      email,
+      password: hashPassword,
+    });
+
+    const token = genrateToken(createUser);
+    res.cookie("token", token);
+
+    res.redirect("/");
+  } catch (error) {
+    debug(`Error creating user ${error.message}`);
+    res.status(500).send("An internal server error occured");
+  }
+};
