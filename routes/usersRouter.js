fb80888a346220c9b84ecac699b99f2dab0d7ddf
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-21 03:33:08 +0200

    feat: add login route to usersRouter for user authentication

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser } = require("../controllers/authControllers");
+const { registerUser, loginUser } = require("../controllers/authControllers");
 
 router.get("/", (req, res) => {
   res.send("Hey, This is working perfectly");
 });
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 module.exports = router;
