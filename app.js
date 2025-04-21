bded3bc8bfdfa89a60adb201ec4e96c68fc6fd80
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-21 04:40:21 +0200

    refactor: remove unused routes and integrate index router for cleaner structure

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const db = require("./config/mongoose-connection");
 const ownersRouter = require("./routes/ownersRouter.js");
 const usersRouter = require("./routes/usersRouter.js");
 const productsRouter = require("./routes/productsRouter.js");
+const index = require("./routes/index.js");
 const expressSession = require("express-session");
 const flash = require("connect-flash");
 
@@ -26,40 +27,10 @@ app.use(flash());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
+app.use("/", index);
 app.use("/owners", ownersRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
-
-/* I am creating some routes for testing out ejs*/
-
-app.get("/", (req, res) => {
-  res.render("index");
-  // Render the index.ejs file
-});
-
-app.get("/header", (req, res) => {
-  res.render("header"); // Render the header.ejs file
-});
-
-app.get("/admin", (req, res) => {
-  res.render("admin"); // Render the admin.ejs file
-});
-
-app.get("/cart", (req, res) => {
-  res.render("cart"); // Render the cart.ejs file
-});
-
-app.get("/create-products", (req, res) => {
-  res.render("createProducts", { success: "" }); // Render the createProducts.ejs file
-});
-
-app.get("/owner-login", (req, res) => {
-  res.render("owner-login"); // Render the owner-login.ejs file
-});
-
-app.get("/shop", (req, res) => {
-  res.render("shop", { products: [] }); // Render the shop.ejs file
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
